5de3eafdc4c25a44ca561952bc2975a4057275c8
villjr <61257261+villjr@users.noreply.github.com>
2020-03-17 17:11:24 -0500

    Add files via upload

---
+++ MediaPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10_4_jorgevillarreal
+
+{
+    class MediaPlayer
+    {
+
+        public void PlayMedia(IAudioPlayer media)
+        {
+            media.Play();
+        }
+    }
+}
